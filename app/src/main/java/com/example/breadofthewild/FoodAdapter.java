1eb58c0b031f97dd76e2c2c47fcd96b37a04e603
Kimmymochi <57956184+Kimmymochi@users.noreply.github.com>
2020-07-08 02:27:25 +0200

    Display list of all foods with description & test image

---
+++ app/src/main/java/com/example/breadofthewild/FoodAdapter.java
@@ -0,0 +1,63 @@
+package com.example.breadofthewild;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.FoodViewHolder> {
+    private Context context;
+    private List<Food> list;
+
+    public FoodAdapter(Context context, List<Food> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    public static class FoodViewHolder extends RecyclerView.ViewHolder {
+        public TextView viewName;
+        public ImageView viewImage;
+        public TextView viewDescription;
+
+
+        public FoodViewHolder(@NonNull View itemView) {
+            super(itemView);
+            viewName = itemView.findViewById(R.id.main_name);
+            viewImage = itemView.findViewById(R.id.main_image);
+            viewDescription = itemView.findViewById(R.id.main_description);
+        }
+    }
+
+    @NonNull
+    @Override
+    public FoodAdapter.FoodViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.single_item, parent, false);
+        FoodViewHolder foodViewHolder = new FoodViewHolder(v);
+        return foodViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FoodAdapter.FoodViewHolder holder, int position) {
+        Food food = list.get(position);
+        holder.viewName.setText(food.getName());
+        holder.viewDescription.setText(food.getDescription());
+//        Picasso.get().load(food.getImage()).into(holder.viewImage);
+        Picasso.get().load("https://dl.airtable.com/JtPShAO4TQS3JtMsg87E_40px-BotW_Tough_Elixir_Icon.png%3Fversion%3D53aabd7829fcba3fc04ea19cd05e31ff").into(holder.viewImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+}
+
--- app/src/main/java/com/example/breadofthewild/MainActivity.java
+++ app/src/main/java/com/example/breadofthewild/MainActivity.java
@@ -1,14 +1,95 @@
 package com.example.breadofthewild;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.lang.reflect.Array;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String url = "https://botw-cookbook-api.herokuapp.com/api/food";
+
+    private RecyclerView mList;
+
+    private LinearLayoutManager linearLayoutManager;
+    private DividerItemDecoration dividerItemDecoration;
+    private List<Food> foodList;
+    private RecyclerView.Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mList = findViewById(R.id.main_list);
+        foodList = new ArrayList<>();
+        adapter = new FoodAdapter(getApplicationContext(), foodList);
+        linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        dividerItemDecoration = new DividerItemDecoration(mList.getContext(), linearLayoutManager.getOrientation());
+        mList.setHasFixedSize(true);
+        mList.setLayoutManager(linearLayoutManager);
+        mList.addItemDecoration(dividerItemDecoration);
+        mList.setAdapter(adapter);
+
+        getData();
+    }
+
+    private void getData () {
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading...");
+        progressDialog.show();
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        JSONObject jsonObject = response.getJSONObject(i);
+                        Food food = new Food();
+                        food.setName(jsonObject.getString("name"));
+                        food.setImage(jsonObject.getString("image"));
+                        food.setDescription(jsonObject.getString("description"));
+                        foodList.add(food);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        progressDialog.dismiss();
+                    }
+                }
+                adapter.notifyDataSetChanged();
+                progressDialog.dismiss();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Volley", error.toString());
+                progressDialog.dismiss();
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(jsonArrayRequest);
     }
 }
