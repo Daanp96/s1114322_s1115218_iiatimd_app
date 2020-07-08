6bc5a87b72127f1852f3d9a55b0cbc3d89cbcefd
Kimmymochi <57956184+Kimmymochi@users.noreply.github.com>
2020-07-08 21:40:44 +0200

    Login Works!

--- app/src/main/java/com/example/breadofthewild/LoginActivity.java
+++ app/src/main/java/com/example/breadofthewild/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.breadofthewild;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,14 +10,34 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.net.CookieHandler;
+import java.net.CookieManager;
+import java.util.HashMap;
+import java.util.Map;
+
 public class LoginActivity extends AppCompatActivity {
 
     EditText user;
     EditText pass;
+    RequestQueue requestQueue;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        CookieManager manager = new CookieManager();
+        CookieHandler.setDefault(manager);
+        requestQueue = Volley.newRequestQueue(this);
+        requestQueue.start();
+
         setContentView(R.layout.activity_login);
         Button toSecondScreen = findViewById(R.id.loginButton);
         toSecondScreen.setOnClickListener(new View.OnClickListener() {
@@ -28,24 +49,34 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void logScreen(){
-        Bundle bundleForSecondScreen = new Bundle();
-
-        String inlog = "Link";
         user = findViewById(R.id.userInput);
-        String name = user.getText().toString();
+        String email = user.getText().toString();
 
-        String passGood = "Ganon?MoreLikeGaynon";
         pass = findViewById(R.id.passInput);
         String password = pass.getText().toString();
+        String url = "https://botw-cookbook-api.herokuapp.com/api/login?email="+email+"&password="+password;
+        final Intent toDashboardScreenIntent = new Intent(this, DashboardActivity.class);
 
-        if(inlog.equals(name) && passGood.equals(password)){
-            bundleForSecondScreen.putString("name", name);
-            Intent toDashboardScreenIntent = new Intent(this, DashboardActivity.class);
-            toDashboardScreenIntent.putExtras(bundleForSecondScreen);
-            startActivity(toDashboardScreenIntent);
-            Toast.makeText(this, "Welcome!", Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(this, "Something went wrong...", Toast.LENGTH_SHORT).show();
-        }
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+//                        getJsonData();
+                        Log.d("RESPONSE", response);
+
+
+                        startActivity(toDashboardScreenIntent);
+
+                    }
+
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(LoginActivity.this, error.toString(), Toast.LENGTH_LONG).show();
+                    }
+                });
+        requestQueue.add(stringRequest);
     }
+
 }
