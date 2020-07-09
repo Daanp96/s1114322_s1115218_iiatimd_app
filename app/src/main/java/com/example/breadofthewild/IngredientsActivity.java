30b9317f7495bbf56dfd55db3a4b4be108761aee
Daan <daan.passchier@xs4all.nl>
2020-07-09 17:57:47 +0200

    Moved ingredients recycler to new activity

---
+++ app/src/main/java/com/example/breadofthewild/IngredientsActivity.java
@@ -0,0 +1,89 @@
+package com.example.breadofthewild;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.util.Log;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IngredientsActivity extends AppCompatActivity {
+
+    private String url = "https://botw-cookbook.herokuapp.com/api/ingredient";
+
+    private RecyclerView mList;
+
+    private LinearLayoutManager linearLayoutManager;
+    private DividerItemDecoration dividerItemDecoration;
+    private List<Ingredient> ingredientList;
+    private RecyclerView.Adapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ingredients);
+
+        mList = findViewById(R.id.main_list);
+        ingredientList = new ArrayList<>();
+        adapter = new IngredientAdapter(getApplicationContext(), ingredientList);
+        linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        dividerItemDecoration = new DividerItemDecoration(mList.getContext(), linearLayoutManager.getOrientation());
+        mList.setHasFixedSize(true);
+        mList.setLayoutManager(linearLayoutManager);
+        mList.addItemDecoration(dividerItemDecoration);
+        mList.setAdapter(adapter);
+
+        getData();
+    }
+
+    private void getData () {
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading...");
+        progressDialog.show();
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        JSONObject jsonObject = response.getJSONObject(i);
+                        Ingredient ingredient = new Ingredient();
+                        ingredient.setName(jsonObject.getString("name"));
+                        ingredient.setImage(jsonObject.getString("image"));
+                        ingredient.setDescription(jsonObject.getString("description"));
+                        ingredient.setSubclass(jsonObject.getString("subclass"));
+                        ingredientList.add(ingredient);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        progressDialog.dismiss();
+                    }
+                }
+                adapter.notifyDataSetChanged();
+                progressDialog.dismiss();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Volley", error.toString());
+                progressDialog.dismiss();
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(jsonArrayRequest);
+    }
+}
--- app/src/main/java/com/example/breadofthewild/MainActivity.java
+++ app/src/main/java/com/example/breadofthewild/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.breadofthewild;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -22,70 +28,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
-//    private String url = "https://botw-cookbook-api.herokuapp.com/api/food";
-    private String url = "https://botw-cookbook.herokuapp.com/api/ingredient";
-
-    private RecyclerView mList;
-
-    private LinearLayoutManager linearLayoutManager;
-    private DividerItemDecoration dividerItemDecoration;
-//    private List<Food> foodList;
-    private List<Ingredient> ingredientList;
-    private RecyclerView.Adapter adapter;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mList = findViewById(R.id.main_list);
-        ingredientList = new ArrayList<>();
-        adapter = new IngredientAdapter(getApplicationContext(), ingredientList);
-        linearLayoutManager = new LinearLayoutManager(this);
-        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-        dividerItemDecoration = new DividerItemDecoration(mList.getContext(), linearLayoutManager.getOrientation());
-        mList.setHasFixedSize(true);
-        mList.setLayoutManager(linearLayoutManager);
-        mList.addItemDecoration(dividerItemDecoration);
-        mList.setAdapter(adapter);
-
-        getData();
-    }
-
-    private void getData () {
-        final ProgressDialog progressDialog = new ProgressDialog(this);
-        progressDialog.setMessage("Loading...");
-        progressDialog.show();
-
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
+        Button button = findViewById(R.id.toIngredients);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onResponse(JSONArray response) {
-                for (int i = 0; i < response.length(); i++) {
-                    try {
-                        JSONObject jsonObject = response.getJSONObject(i);
-                        Ingredient ingredient = new Ingredient();
-                        ingredient.setName(jsonObject.getString("name"));
-                        ingredient.setImage(jsonObject.getString("image"));
-                        ingredient.setDescription(jsonObject.getString("description"));
-                        ingredient.setSubclass(jsonObject.getString("subclass"));
-                        ingredientList.add(ingredient);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                        progressDialog.dismiss();
-                    }
-                }
-                adapter.notifyDataSetChanged();
-                progressDialog.dismiss();
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e("Volley", error.toString());
-                progressDialog.dismiss();
+            public void onClick(View v) {
+                goToIngredients();
             }
         });
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-        requestQueue.add(jsonArrayRequest);
+    }
+
+    public void goToIngredients(){
+        Intent loginIntent = new Intent(this, IngredientsActivity.class);
+        startActivity(loginIntent);
     }
 }
