a0f3e5736dfc9dede42052060d9ddabea8b44468
Daan <daan.passchier@xs4all.nl>
2020-07-09 21:43:49 +0200

    Updated styling for ingredient recycler

--- app/src/main/java/com/example/breadofthewild/MainActivity.java
+++ app/src/main/java/com/example/breadofthewild/MainActivity.java
@@ -1,26 +1,14 @@
 package com.example.breadofthewild;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.Volley;
+import androidx.appcompat.app.AppCompatActivity;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.List;
-
+public class MainActivity extends AppCompatActivity {
     MediaPlayer mp;
 
     @Override
@@ -32,6 +20,7 @@ import java.util.List;
         mp.seekTo(0);
         mp.setVolume(0.5f, 0.5f);
         toLoginScreen.setOnClickListener(new View.OnClickListener(){
+
             @Override
             public void onClick(View v) {
                 toLoginActivity();
